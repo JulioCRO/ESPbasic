6ca6f17201979e8bb4c1b488d17fd6a836cc1380
Julio Rodrigues <julio.c.rodrigues@accenture.com>
2024-04-22 11:26:27 -0300

    Reestrutura

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <ESPbasic.h>
+
+
+void setup() {
+/*defaul start*/    
+startBasic();
+
+//ADD http service
+/*
+serviceAdd("/service", HTTP_ANY, [&](AsyncWebServerRequest *request) {
+            request->send(200, "text/html", serviceList );
+});	
+*/
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
